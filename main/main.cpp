a1a54510547bc1c842ae30782223664855afe8cc
Matthew Nickson <mnickson@sidingsmedia.com>
2023-02-04 14:54:40 +0000

    Added compile time deffinitions
    These have been added to the main components CMakeLists.txt as for some
    reason, it does not work when they are added to the root one.
    
    Also added log message with compile time and version.
    
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- main/main.cpp
+++ main/main.cpp
@@ -7,8 +7,11 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "timekeeping/clock.hpp"
+
 extern "C" void app_main() {
-    ESP_LOGI("APP_MAIN", "Hello world");
+    ESP_LOGI("STARTUP", "%s %s", PROJECT_NAME, PROJECT_VERSION);
+    ESP_LOGI("STARTUP", "Compiled %s %s", __DATE__, __TIME__);
 
     // Log chip info
     esp_chip_info_t chip_info;
@@ -17,8 +20,11 @@ extern "C" void app_main() {
     ESP_LOGI("APP_MAIN", "Silicon revision %d", chip_info.revision);
     ESP_LOGI("APP_MAIN", "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    timekeeping::Clock clock(1675512811);
     for (int i = 10; i >= 0; i--) {
         ESP_LOGI("APP_MAIN", "Restarting in %d seconds...\n", i);
+        clock.Now();
+        ESP_LOGI("TIME", "Formatted time %d:%d:%d", clock.Hour(), clock.Minute(), clock.Second());
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 
